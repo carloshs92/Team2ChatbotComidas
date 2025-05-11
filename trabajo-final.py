31a246c36f40782faaf2f939233734780e0e3bcd
Carlos Huamani <chuamani@franquiciasperu.com>
2025-05-10 20:18:56 -0500

    feat: server

--- trabajo-final.py
+++ trabajo-final.py
@@ -109,7 +109,7 @@ def whatsapp_mymessage():
     precio_keywords = ["precio", "cuánto cuesta", "cual es el costo", "cuanto vale"]
 
     try:
-        if any(keyword in incoming_msg for keyword in precio_keywords):
+        if any(keyword in incoming_msg.lower() for keyword in precio_keywords):
             producto = extraer_electro(incoming_msg)
             print("Producto extraído:", producto)
             if producto:
